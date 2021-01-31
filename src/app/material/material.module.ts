cd3a41b04dfd93a892ab297f63e688ca9a277ba3
NaudtsRuben <47577203+NaudtsRuben@users.noreply.github.com>
2021-01-31 17:47:36 +0100

    fix styling bug projects

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatNavList, MatListModule } from '@angular/material/list';
-import { MatIconModule, MatIcon } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { ReactiveFormsModule } from '@angular/forms';
-import { SlideshowModule } from 'ng-simple-slideshow';
 import { MatCarouselModule } from '@ngmodule/material-carousel';
-import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 
